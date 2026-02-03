9c2a59e556c6e8bd5f888c27ed8a8e1aa31da6c7
Frida <fridapalms@live.se>
2026-02-03 15:54:31 +0100

    fixed projects

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { Project } from "./models/Project";
 import "./style.scss";
 
+const innekatt: Project = new Project("Innekatt", "I designed this webshop in Figma and developed it using HTML, SCSS and TypeScript", "/innekatt.png", "https://github.com/fridapalms/innekatt-webshop.git", "https://fridapalms-innekatt.vercel.app/");
+const coffeeshop: Project = new Project("Coffeeshop", "Group project from JavaScript course at Medieinstitutet, creating a webshop", "/coffeeshop.png", "https://github.com/fridapalms/coffee-webshop.git", "https://fridapalms-coffeeshop.vercel.app/");
 const moviefinder: Project = new Project("Movie Finder", "Using OMDB API, search for movies, read about them and add them to your watchlist", "/moviefinder.png", "https://github.com/fridapalms/movie-finder.git", "https://fridapalms-movie-finder.vercel.app");
 const todolist: Project = new Project("To do-list", "Assignment from JavaScript course at Medieinstitutet, creating a to do-list", "/todolist.png", "https://github.com/fridapalms/the-todos.git", "https://fridapalms.github.io/the-todos/");
-const thirdproject: Project = new Project("Coffeeshop", "Group project from JavaScript course at Medieinstitutet, creating a webshop", "/coffeeshop.png", "https://github.com/fridapalms/coffee-webshop.git", "https://fridapalms-coffeeshop.vercel.app/");
 
-const projects: Project[] = [moviefinder, todolist, thirdproject];
+const projects: Project[] = [innekatt, coffeeshop, moviefinder, todolist];
 
 projects.forEach((project) => {
   const work = document.getElementById("workprojects");
